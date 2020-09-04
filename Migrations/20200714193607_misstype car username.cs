b36af0ae7ace9bb916d2780d81a2f31a1c751224
NoPalm0il <jpedrocr2000@hotmail.com>
2020-09-04 14:46:52 +0100

    Updated views, removed reservation date

---
+++ Migrations/20200714193607_misstype car username.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IParkT_Authentication.Migrations
+{
+    public partial class misstypecarusername : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "username",
+                table: "Car",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "username",
+                table: "Car",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
---
+++ Migrations/20200714195207_rename carId to LicensePlate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IParkT_Authentication.Migrations
+{
+    public partial class renamecarIdtoLicensePlate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CarId",
+                table: "Reservation");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LicensePlate",
+                table: "Reservation",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LicensePlate",
+                table: "Reservation");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CarId",
+                table: "Reservation",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
