0f8190049a7269757f1f3d1c58c3c4bf427a25af
jjacintoc <62345038+jjacintoc@users.noreply.github.com>
2020-09-15 05:01:52 +0100

    Atualização da base de dados dos lugares, mudanças visuais no website, inicio do dark theme TODO: acabar dark theme

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var nightModeToggleButton = document.querySelector(".nightModeButton");
+var darktogglehead = document.querySelector(".lighttheme");
+
+nightModeToggleButton.onclick = function () {
+    nightModeToggleButton.classList.toggle("night-mode");
+    darktogglehead.classList.toggle("night-mode");
+};
